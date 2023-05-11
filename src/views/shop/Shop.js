04650e576eecc44bc5a5fb3e2646e1188f8fd4fe
Katrina <kalynndier@gmail.com>
2023-05-11 16:16:12 -0400

    practice

--- src/views/shop/Shop.js
+++ src/views/shop/Shop.js
@@ -4,12 +4,12 @@ import Footer from '../../components/footer/Footer'
 import '../shop/shop.scss'
 
 const Shop = () => {
+
+
   return (
     <section className='shop'>
     <ShopNav />
-      <main>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et blanditiis distinctio voluptatum ex tenetur itaque reprehenderit possimus aspernatur magnam accusantium eos eveniet cum dolor corporis necessitatibus nisi, optio fugit tempora?</p>
-      </main>
+    
       <Footer />
     
     </section>
